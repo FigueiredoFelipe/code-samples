9703649b2dad1fc26174d4ef1d3aed7c735e9894
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2023-02-25 18:24:40 -0300

    fix: variables
    changed

--- javascript/01-array-sum-even/array-even-sum-filter-reduce.js.js
+++ javascript/01-array-sum-even/array-even-sum-filter-reduce.js.js
@@ -4,9 +4,9 @@
 
 const numbers = [1,2,3,4,5,6,7,8,9,10]
 
-function sumEvenNumbers (nam){
+function sumEvenNumbers (num){
 
-    const evenNumbersSum = (numbers.filter(nam => nam % 2 === 0)).reduce((acc, nam)=>acc + nam, 0)
+    const evenNumbersSum = (num.filter(num => num % 2 === 0)).reduce((acc, num)=>acc + num, 0)
     console.log(`The sum of all even numbers is: ${evenNumbersSum}`);
 
     return evenNumbersSum
