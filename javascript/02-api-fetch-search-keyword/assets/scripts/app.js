da25f7069cd81f12622e59f260a1067213ef92c2
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2023-02-27 13:37:35 -0300

    feat: sample-02
    api

--- javascript/02-api-fetch-search-keyword/assets/scripts/app.js
+++ javascript/02-api-fetch-search-keyword/assets/scripts/app.js
@@ -11,22 +11,23 @@
 
 const movies = []
 
-async function fetchDataAwait(){
+async function fetchDataAwait() {
     const response = await fetch('https://swapi.dev/api/films/')
     const data = await response.json()
-    
+
     return data
 }
 
 
 const titles = fetchDataAwait;
+const displayMovie = document.getElementById('movies-list')
 
 
 titles().then(data => {
-    const displayMovie = document.getElementById('movies-list')
     const results = data.results
-    results.map((movie)=>{
+    results.map((movie) => {
         movies.push(movie)
+
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="card">
@@ -42,10 +43,8 @@ titles().then(data => {
         `
         displayMovie.appendChild(div)
     })
-    console.log(movies);
-
-
 }).catch(error => console.log(error))
+
 
 
 
@@ -53,12 +52,33 @@ titles().then(data => {
 const input = document.querySelector('#inp')
 const button = document.getElementById('fetch-btn')
 
-console.log(button);
 
-function test(){
-    console.log(input.value);
-    console.log(movies);
-    movies.filter()
+function handleFilter() {
+
+    const filterInput = input.value.trim().toLowerCase()
+    const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(filterInput))
+
+    console.log(filteredMovies);
+
+    displayMovie.innerHTML = ''
+
+    filteredMovies.map((movie) => {
+        const div = document.createElement('div')
+        div.innerHTML =
+            `
+        <div class="card">
+        <div class="card-title">
+            <h1>${movie.title}</h1>
+        </div>
+        <div class="card-body">
+            <p class="opening_crawl">${movie.opening_crawl}
+            </p>
+            <p class="release_date">Release Date: ${movie.release_date}</p>
+        </div>
+        </div>
+        `
+        displayMovie.appendChild(div)
+    })
 }
 
-button.addEventListener('click', test)
+button.addEventListener('click', handleFilter)
